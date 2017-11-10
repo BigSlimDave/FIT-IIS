f0290962572f2943f3b7779af105569edaeeab16
Unknown <david-smekal@hotmail.cz>
2017-11-10 22:47:08 +0100

    xxx

--- app/profil/views.py
+++ app/profil/views.py
@@ -2,7 +2,7 @@
 
 from flask import Blueprint, render_template, abort, request, session, redirect, flash, url_for
 from jinja2 import TemplateNotFound
-from app.functions import login_required, loginForm, database
+from app.functions import *
 
 profil = Blueprint('profil', __name__, static_folder='static', template_folder='templates')
 
@@ -10,5 +10,15 @@ profil = Blueprint('profil', __name__, static_folder='static', template_folder='
 @login_required('user')
 def index():
     account = session
-    return render_template('profil/index.html', account=account, table='')
+    uzivatel = db_get_from_where_one('uzivatele', "prezdivka='{0}'".format(session['username']), ['*'])
+    hrac = db_get_from_where_one('hrac', "prezdivka='{0}'".format(session['username']), ['*'])
+    vybaveni = db_get_from_where_all('vybaveni', "vlastnik='{0}'".format(hrac[0]), ['*'])
+    tym = db_get_from_where_one('tym_clenstvi', "hrac='{0}'".format(hrac[0]), ['id'])
+    if tym != None:
+        tym = db_get_from_where_one('tym', "id='{0}'".format(id_tym), ['*'])
+    klan = db_get_from_where_one('klan_clenstvi', "hrac='{0}'".format(hrac[0]), ['id'])
+    if klan != None:
+        klan = db_get_from_where_one('klan', "id='{0}'".format(id_tym), ['*'])
+    specializace = db_get_from_where_one('specializace', "hrac='{0}'".format(hrac[0]), ['hra'])
+    return render_template('profil/index.html', account=account, uzivatel=uzivatel, hrac=hrac, vybaveni=vybaveni, tym=tym, klan=klan, specializace=specializace)
 
